d3d3c59fd28bc1b9ab405c0ff154f02b65524b7d
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2023-08-13 09:21:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const CHAT_BUTTON_WIDTH = 120; // width of the chat button in pixels
 const CHAT_BUTTON_HEIGHT = 50; // height of the chat button in pixels
-const CHAT_BUTTON_BACKGROUND_COLOR = 'black'; // background color of the chat button
+const CHAT_BUTTON_BACKGROUND_COLOR = '#333'; // darker background color for contrast
 const CHAT_BUTTON_BORDER_RADIUS = '15px'; // rounded edges for the rectangle
-const scriptTag = document.currentScript;
 
 const CHAT_BUTTON_ICON = `
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -16,8 +15,9 @@ const chatButton = document.createElement('div');
 // Apply styles to the chat button
 chatButton.setAttribute('id', 'chatbase-bubble-button');
 chatButton.style.position = 'fixed';
-chatButton.style.bottom = '20px';
-chatButton.style.right = '20px';
+chatButton.style.bottom = '50%'; // Center the button vertically
+chatButton.style.right = '50%'; // Center the button horizontally
+chatButton.style.transform = 'translate(50%, 50%)'; // Ensure true centering by translating by half of its size
 chatButton.style.width = CHAT_BUTTON_WIDTH + 'px';
 chatButton.style.height = CHAT_BUTTON_HEIGHT + 'px';
 chatButton.style.borderRadius = CHAT_BUTTON_BORDER_RADIUS;
@@ -31,47 +31,51 @@ chatButton.style.alignItems = 'center';
 chatButton.style.justifyContent = 'space-around';
 chatButton.innerText = "Chat"; // Add a label
 
-chatButton.addEventListener('mouseenter', (event) => {
-  chatButton.style.transform = 'scale(1.05)';
-});
-chatButton.addEventListener('mouseleave', (event) => {
-  chatButton.style.transform = 'scale(1)';
-});
-
 // Create the chat button icon element
 const chatButtonIcon = document.createElement('div');
 chatButtonIcon.innerHTML = CHAT_BUTTON_ICON;
 chatButton.appendChild(chatButtonIcon);
 
-// Toggle the chat component when the chat button is clicked
-chatButton.addEventListener('click', () => {
-  if (chat.style.display === 'none') {
-    chat.style.display = 'flex';
-  } else {
-    chat.style.display = 'none';
-  }
-});
-
+// Chat Screen
 const chat = document.createElement('div');
 chat.setAttribute('id', 'chatbase-bubble-window');
-
 chat.style.position = 'fixed';
 chat.style.top = '0';
 chat.style.left = '0';
 chat.style.width = '100vw';
 chat.style.height = '100vh';
 chat.style.background = 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)'; // Gradient background
-chat.style.display = 'none';
 chat.style.zIndex = 999999999;
 chat.style.overflow = 'hidden';
 chat.style.justifyContent = 'center';
 chat.style.alignItems = 'center';
-chat.style.display = 'flex';
+chat.style.display = 'none'; // Hidden by default
 chat.style.flexDirection = 'column';
 
+const lilithLabel = document.createElement('span');
+lilithLabel.innerText = "Lilith";
+lilithLabel.style.color = 'white';
+lilithLabel.style.fontSize = '24px';
+lilithLabel.style.marginBottom = '20px';
+chat.appendChild(lilithLabel);
+
+const iframe = document.createElement('iframe');
+iframe.src = "chat.html"; // Placeholder URL for the chat content
+iframe.width = "80%";
+iframe.height = "70%";
+iframe.frameBorder = "0";
+chat.appendChild(iframe);
+
 document.body.appendChild(chat);
-
-chat.innerHTML = `<span style="color: white; font-size: 24px; margin-bottom: 20px;">Lilith</span>
-<iframe src="${scriptTag.id}" width="80%" height="70%" frameborder="0"></iframe>`;
-
 document.body.appendChild(chatButton);
+
+// Toggle the chat component when the chat button is clicked
+chatButton.addEventListener('click', () => {
+  if (chat.style.display === 'none' || chat.style.display === '') {
+    chat.style.display = 'flex';
+    chatButton.style.display = 'none'; // Hide the chat button
+  } else {
+    chat.style.display = 'none';
+    chatButton.style.display = 'flex'; // Show the chat button
+  }
+});
