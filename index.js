ee0cbe66e2de042ca48749783912806a51474c8e
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2023-08-13 09:16:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const CHAT_BUTTON_WIDTH = 180; // width of the chat button in pixels
+const CHAT_BUTTON_WIDTH = 120; // width of the chat button in pixels
 const CHAT_BUTTON_HEIGHT = 50; // height of the chat button in pixels
 const CHAT_BUTTON_BACKGROUND_COLOR = 'black'; // background color of the chat button
-const CHAT_BUTTON_BORDER_RADIUS = '25px'; // radius of the chat button in pixels
+const CHAT_BUTTON_BORDER_RADIUS = '15px'; // rounded edges for the rectangle
 const scriptTag = document.currentScript;
+
 const CHAT_BUTTON_ICON = `
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H6L4 18V4H20V16Z" fill="#FFFFFF"/>
   </svg>
 `;
 
-// create the chat button element
+// Create the chat button element
 const chatButton = document.createElement('div');
-// apply styles to the chat button
+
+// Apply styles to the chat button
 chatButton.setAttribute('id', 'chatbase-bubble-button');
 chatButton.style.position = 'fixed';
 chatButton.style.bottom = '20px';
@@ -27,7 +29,7 @@ chatButton.style.transition = 'all .2s ease-in-out';
 chatButton.style.display = 'flex';
 chatButton.style.alignItems = 'center';
 chatButton.style.justifyContent = 'space-around';
-chatButton.innerHTML = "Chat with us!"; // Add a label next to the icon
+chatButton.innerText = "Chat"; // Add a label
 
 chatButton.addEventListener('mouseenter', (event) => {
   chatButton.style.transform = 'scale(1.05)';
@@ -36,19 +38,13 @@ chatButton.addEventListener('mouseleave', (event) => {
   chatButton.style.transform = 'scale(1)';
 });
 
-// create the chat button icon element
+// Create the chat button icon element
 const chatButtonIcon = document.createElement('div');
-
-// apply styles to the chat button icon
-chatButtonIcon.style.zIndex = 999999999;
 chatButtonIcon.innerHTML = CHAT_BUTTON_ICON;
 chatButton.appendChild(chatButtonIcon);
 
-// add the chat button to the page
-
-// toggle the chat component when the chat button is clicked
+// Toggle the chat component when the chat button is clicked
 chatButton.addEventListener('click', () => {
-  // toggle the chat component
   if (chat.style.display === 'none') {
     chat.style.display = 'flex';
   } else {
@@ -60,48 +56,22 @@ const chat = document.createElement('div');
 chat.setAttribute('id', 'chatbase-bubble-window');
 
 chat.style.position = 'fixed';
-chat.style.flexDirection = 'column';
-chat.style.justifyContent = 'space-between';
-chat.style.bottom = '80px';
-chat.style.right = '20px';
-chat.style.width = '85vw';
-chat.style.height = '70vh';
-chat.style.boxShadow = 'rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px';
+chat.style.top = '0';
+chat.style.left = '0';
+chat.style.width = '100vw';
+chat.style.height = '100vh';
+chat.style.background = 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)'; // Gradient background
 chat.style.display = 'none';
-chat.style.borderRadius = '15px'; // Increased border radius for better aesthetics
 chat.style.zIndex = 999999999;
 chat.style.overflow = 'hidden';
-chat.style.background = 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)'; // Add a gradient for visual appeal
+chat.style.justifyContent = 'center';
+chat.style.alignItems = 'center';
+chat.style.display = 'flex';
+chat.style.flexDirection = 'column';
 
 document.body.appendChild(chat);
 
-chat.innerHTML = `<iframe
-src="${scriptTag.id}"
-width="100%"
-height="100%"
-frameborder="0"
-></iframe>`;
+chat.innerHTML = `<span style="color: white; font-size: 24px; margin-bottom: 20px;">Lilith</span>
+<iframe src="${scriptTag.id}" width="80%" height="70%" frameborder="0"></iframe>`;
 
-// Create a condition that targets viewports at least 768px wide
-const mediaQuery = window.matchMedia('(min-width: 550px)');
-
-function handleChatWindowSizeChange(e) {
-  // Check if the media query is true
-  if (e.matches) {
-    chat.style.height = '600px';
-    chat.style.width = '400px';
-  }
-}
-
-// Register event listener
-mediaQuery.addEventListener('change', handleChatWindowSizeChange);
-
-// Initial check
-handleChatWindowSizeChange(mediaQuery);
-
-const getChatButtonColor = async () => {
-  chatButton.style.backgroundColor = CHAT_BUTTON_BACKGROUND_COLOR;
-  document.body.appendChild(chatButton);
-}
-
-getChatButtonColor();
+document.body.appendChild(chatButton);
